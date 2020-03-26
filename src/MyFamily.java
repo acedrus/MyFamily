22ebd2faabd95f350a84b816b1eb2ddbf0ac588e
acedrus <acedrus@mail.ru>
2020-03-26 08:56:29 +0300

    Initial commit

---
+++ src/MyFamily.java
@@ -0,0 +1,7 @@
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("Альберт");
+        System.out.println("Ангелина");
+        System.out.println("Василий");
+    }
+}
